9acdb406ee75ff084a94c0f073b3cb86d66e9cf5
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-07-25 12:23:51 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
+  constructor() {
+    super()
+/*     const element = <h1>Hello</h1>;
+    console.log(element) */
+    this.loggedInStudent = 'Pam'
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  displayLoggedInStudent() {
+    if (this.loggedInStudent) {
+      return <h1>Welcome, { this.loggedInStudent }</h1>
+    } else {
+      return <h1>There is no one logged in.</h1>
+    }
+  }
+
+  displayStudents() {
+    const students = ["Lucas", "Chris", "Jake"]
+
+    // Approach 1: With a for loop
+    for (let i = 0; i < students.length; i++) {
+      students[i] = <li>{ students[i] }</li>
+    }
+
+    const element = (
+      <ul>
+        {students}
+      </ul>
+    )
+
+    // Approach 2: With the map method
+/*     const element = (
+      <ul>
+        { students.map((student) => <li>{ student }</li>) }
+      </ul>
+    ) */
+
+    console.log(element)
+
+    return element;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* { this.displayLoggedInStudent() } */}
+        {
+          // Ternary operator:
+          // condition ?
+          // output if true :
+          // output if false
+          (this.loggedInStudent) ?
+          (<h1>Welcome, { this.loggedInStudent }</h1>) :
+          (<h1>There is no one logged in.</h1>)
+        }
+        { this.displayStudents() }
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
